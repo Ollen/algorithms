29a0ae54b3ab15d286edcd0c11e7b0a2e5c531b9
Allendale Nato <natoallendale@gmail.com>
2017-09-03 21:50:14 +0800

    Add modular fizz buzz in js

--- fizz buzz/fizz_buzz.js
+++ fizz buzz/fizz_buzz.js
@@ -1,6 +1,5 @@
 function fizzBuzz(num) {
-    
-    for (let i = 0; i < num; i++) {
+    for (let i = 1; i <= num; i++) {
         let result = '';
 
         if (i % 3 === 0) {
@@ -16,4 +15,23 @@ function fizzBuzz(num) {
     }
 }
 
+/**
+ * Modular Fizz Buzz
+ * @param {Number} num 
+ */
+function modularFizzBuzz(num, fizz=3, buzz=5) {
+    for (let i = 1; i <= num; i++) {
+        if (i % fizz === 0 && i % buzz === 0) {
+            console.log('FizzBuzz')
+        } else if (i % fizz === 0) {
+            console.log('Fizz');
+        } else if (i % buzz === 0) {
+            console.log('Buzz');
+        } else {
+            console.log(i);
+        }
+    }
+}
+
 fizzBuzz(20);
+modularFizzBuzz(20)
