5fb07d43eeeb1a0c0d776700a921ab6458813708
Allendale Nato <natoallendale@gmail.com>
2017-09-05 13:40:04 +0800

    Change forEach() function in ransom note to every()

--- ransom note/ransom_note.js
+++ ransom note/ransom_note.js
@@ -1,3 +1,14 @@
+/**
+ * Ransom Note
+ * 1. Split the input into arrays.
+ * 2. Check if noteArr is > than magArr.
+ * 3. Create a number of words lookup table using the magArr.
+ * 4. Check the noteArr alongside the lookup table.
+ * 
+ * @param {String} noteText 
+ * @param {String} magazineText 
+ */
+
 function ransomNote(noteText, magazineText) {
     let noteArr = noteText.toLowerCase().split(' ');
     let magArr = magazineText.toLowerCase().split(' ');
@@ -18,15 +29,18 @@ function ransomNote(noteText, magazineText) {
 
     // Loop the note array and decrement
     // The value in the lookup table.
-    noteArr.forEach(word => {
+    return noteArr.every(word => {
         if (!magTable[word]) {
             return false;
         }
         magTable[word]--;
+        return true;
     });
 
-    return true;
+    // NOTE: Array.prototype.forEach() is not used in noteArr checking
+    // because there is no way to stop of break a forEach() loop other than throwing an exception.
 }
 
-let a = ransomNote('To be or', 'To be or not to be that is the question');
+let a = ransomNote('To be or not to be that is the question', 
+'puerto rico is a place of great wonder and excitement it has many secrect waterfall locatoins that i am an admirer of you must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip i have for you is to go early in the morning when it is not so hot out note that you must wear hiking boots this is one of the best places i have ever visited');
 console.log(a);
