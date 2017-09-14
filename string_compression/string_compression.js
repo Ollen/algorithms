a4bd15ede03724a62dd7c5541e2b20f7019cf42e
Allendale Nato <natoallendale@gmail.com>
2017-09-14 22:18:52 +0800

    Update stiring_compression

--- string_compression/string_compression.js
+++ string_compression/string_compression.js
@@ -1,4 +1,4 @@
-function compress_string(str) {
+function compress_string_1(str) {
     let res = '';
     let charCount = 0;
     
@@ -13,5 +13,22 @@ function compress_string(str) {
     return res.length < str.length ? res: str;
 }
 
-console.log(compress_string('aaabbabbbbssheaaaoo'));
-console.log(compress_string('abbb'));
+function compress_string_2(str) {
+    let res = [];
+    let charCount = 0;
+    
+    for(let i = 0; i < str.length; i++) {
+        charCount++;
+        let currChar = str.charAt(i);
+        if (currChar != str.charAt(i + 1) || i + 1 == str.length) {
+            res.push(currChar + charCount);
+            charCount = 0;
+        }
+    }
+    return res.join('').length < str.length ? res.join(''): str;
+}
+
+console.log(compress_string_1('aaabbabbbbssheaaaoo'));
+console.log(compress_string_2('abbb'));
+console.log(compress_string_2('aaabbabbbbssheaaaoo'));
+
