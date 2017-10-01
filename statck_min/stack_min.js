a93ff1593078de63ff4f9fffc9515b1e92a4f4a8
Allendale Nato <natoallendale@gmail.com>
2017-10-01 23:04:27 +0800

    Add stack_min 2/2

--- statck_min/stack_min.js
+++ statck_min/stack_min.js
@@ -13,9 +13,26 @@ function push(val) {
     } else {
         res.min = stack[stack.length - 1].min;
     }
+    stack.push(res);
 }
 
-push(2);
-console.log(stack);
+function pop() {
+    let res = stack.pop();
+    console.log(res.val);
+}
 
+function min() {
+    let res = stack[stack.length - 1].min;
+    console.log(res);
+}
+
+push(5);
+push(32);
+push(4);
+push(2);
+min();
+pop();
+min();
+pop();
+min();
 
